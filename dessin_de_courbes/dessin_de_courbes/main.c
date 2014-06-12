748929c719dcbba8f16bc20b134a9259736a9d50
Etienne Ischer <etienne.ischer@gmail.com>
2014-06-12 18:50:59 +0200

    First commit. Creation du projet

---
+++ dessin_de_courbes/dessin_de_courbes/main.c
@@ -0,0 +1,18 @@
+/* 
+Auteur: Etienne Ischer & Thomas Jolain
+Date :
+Titre : Travail Personnel
+But : Dessin de courbes
+*/
+
+// En-têtes à inclure
+#include <stdio.h>
+#include <math.h>
+
+// Programme principal
+int main(){
+	
+	
+
+	return 0;
+}
