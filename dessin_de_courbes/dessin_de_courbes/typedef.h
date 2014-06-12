74179791a923056eafbfe82348c8c4d6c4ebeac0
Thomas Jolain <thomas.jolain@gmail.com>
2014-06-12 20:09:55 +0200

    struct created:  sBitmapFileHeader  sRGB  sBitmapDataHeader

--- dessin_de_courbes/dessin_de_courbes/typedef.h
+++ dessin_de_courbes/dessin_de_courbes/typedef.h
@@ -7,3 +7,34 @@ But :
 -----
 This file contains every single struct and typedef necessary.
 */
+
+typedef struct
+{
+	char B;
+	char V;
+	char R;
+} sRGB;
+
+typedef struct
+{
+	unsigned char bfType[2];
+	unsigned char bfSize[4];
+	unsigned char bfReserved1[2];
+	unsigned char bfReserved2[2];
+	unsigned char bfOffBits[4];
+} sBitmapFileHeader;
+
+typedef struct
+{
+	unsigned char bmpSize[4];
+	unsigned char bmpWidth[4];
+	unsigned char bmpHeight[4];
+	unsigned char bmpPlanes[2];
+	unsigned char bmpBitCount[2];
+	unsigned char bmpCompression[4];
+	unsigned char bmpSizeImage[4];
+	unsigned char bmpPixelPerMeterX[4];
+	unsigned char bmpPixelPerMeterY[4];
+	unsigned char bmpColorUsed[4];
+	unsigned char bmpImportantColor[4];
+} sBitmapDataHeader;
